917aa3a129189e5594ef73c16e1ffd4765775b4c
Pete Hudgins <pete.hudgins@proton.me>
2025-07-11 09:45:42 -0400

    Add markup and styles to header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,7 +1,27 @@
+import { Link, NavLink } from 'react-router-dom';
+
+import './header.css';
 
 const Header = () => {
   return (
-    <header>Header  | links | to | come</header>
+    <header className="header-container">
+      <div className="header-content">
+        <div className="header-name">
+          <Link to="/">
+            Peter Hudgins
+          </Link>
+        </div>
+        <div className='header-nav'>
+          <nav>
+            <ul>
+              <li><NavLink to="/about">About Me</NavLink></li>
+              <li><NavLink to="/resume">Resume</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 };
 
