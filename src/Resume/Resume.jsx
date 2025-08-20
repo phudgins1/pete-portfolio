3f64e1bf9f87de26f89d8f136d42568428e4be4e
Pete Hudgins <pete.hudgins@proton.me>
2025-08-20 09:39:54 -0400

    Add initial Resume page, styling

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -38,8 +38,33 @@ const Resume = () => {
       <article className='resume-summary'>
         <h3>Summary</h3>
         <p>
-          I am really good at doing things, and will replace this text with an awesome explanation of how I do that.
+          I’ve worked in a breadth of roles in enterprise web development, specializing in Cloud Architecture, SQL, React, TypeScript, C#, Elasticsearch, Jest, JavaScript, Redis, Ruby on Rails, Node.js and PostgresSQL.
         </p>
+      </article>
+      <article className='resume-work-experience'>
+        <h3>Work Experience</h3>
+        <div className='work-experience'>
+          <h4>Full Stack Developer</h4>
+          <p className='experience-company'>CoStar Group — Norfolk, VA</p>
+          <p className='experience-dates'>Aug 2021 — Jun 2024</p>
+          <ul className='experience-details'>
+            <li>Optimized internal sales tool functionality by implementing back-end service endpoints for new Homes.com data.</li>
+            <li>Integrated data and business logic for Homes.com sales products.</li>
+            <li>Enhanced user experience of internal sales tool by creating and integrating micro front-ends.</li>
+            <li>Implemented all features using Azure Devops pipelines to deploy code to multiple environments.</li>
+          </ul>
+          <ul className='experience-skills'>
+            <li>C#</li>
+            <li>ElasticSearch</li>
+            <li>MSSQL</li>
+            <li>Typescript</li>
+            <li>ReactJS</li>
+            <li>NodeJS</li>
+            <li>TailwindCSS</li>
+            <li>Jest</li>
+            <li>Cypress</li>
+          </ul>
+        </div>
       </article>
     </section>
   );
